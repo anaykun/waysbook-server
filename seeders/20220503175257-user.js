58a451395cf3aa4f59abc72a3e94f637e3bc292e
irfan <is.irfan2m@gmail.com>
2022-05-22 00:01:43 +0700

    one

---
+++ seeders/20220503175257-user.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          email: "admin@mail.com",
+          password:
+            "$2b$10$CE/Vh2iaV6IZFLyRQWNZguEkzSPEF5pSoikt9tHS67FE9PxpZV7wi", //12345678
+          name: "admin",
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
